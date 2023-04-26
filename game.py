cea104c7aa4a80019c57c942d618458516dc8130
Lucca Rangel Haddad <haddad@Luccas-MacBook-Air.local>
2023-04-26 02:10:05 -0300

    game docstrings

--- game.py
+++ game.py
@@ -4,7 +4,14 @@ from paths import VENV
 from scripts.setup import setup
 
 if __name__ == '__main__':
+    """
+    Main entry point for the package management system.
+    """
+
     if not VENV.is_dir():
+        """
+        If the virtual environment directory does not exist, set it up.
+        """
         setup()
 
     parser = ArgumentParser(
@@ -28,15 +35,27 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.cmd == 'setup':
+        """
+        If the command is 'setup', execute the setup command.
+        """
         setup(setup_parser)
 
     if args.cmd == 'run':
+        """
+        If the command is 'run', run the game with the specified arguments.
+        """
         __import__('scripts.run').run.run(run_parser)
 
     args = parser.parse_args()
 
     try:
+        """
+        Execute the function specified by the arguments.
+        """
         args.func(args)
     except KeyboardInterrupt:
+        """
+        If the user interrupts the program with Ctrl+C, exit with status code 1.
+        """
         print('Aborted.')
         exit(1)
